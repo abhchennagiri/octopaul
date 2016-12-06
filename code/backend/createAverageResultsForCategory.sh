5f86a97878cd42349458370d0d370aed2b9bfda6
Tobias Korfmacher <github@thekorfi.com>
2016-12-05 21:46:41 -0800

    add script for creating average Errors for each category; add Runner for all categorys

---
+++ code/backend/createAverageResultsForCategory.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+dataPath="../../results"
+
+echo "Automotive"
+python createFileInformation.py Automotive $dataPath/Automotive
+
+# echo "AppsForAndroid"
+# python createFileInformation.py AppsForAndroid $dataPath/AppsForAndroid
+
+echo "Baby"
+python createFileInformation.py Baby $dataPath/Baby
+
+echo "Beauty"
+python createFileInformation.py Beauty $dataPath/Beauty
+
+echo "CellPhones"
+python createFileInformation.py CellPhones $dataPath/CellPhones
+
+echo "ClothingShoesJewelry"
+python createFileInformation.py ClothingShoesJewelry $dataPath/ClothingShoesJewelry
+
+# echo "DigitalMusic"
+# python createFileInformation.py DigitalMusic $dataPath/DigitalMusic
+
+# echo "GroceryGourmetFood"
+# python createFileInformation.py GroceryGourmetFood $dataPath/GroceryGourmetFood
+
+echo "Health"
+python createFileInformation.py Health $dataPath/Health
+
+echo "HomeKitchen"
+python createFileInformation.py HomeKitchen $dataPath/HomeKitchen
+
+echo "KindleStore"
+python createFileInformation.py KindleStore $dataPath/KindleStore
+
+# echo "MoviesTV"
+# python createFileInformation.py MoviesTV $dataPath/MoviesTV
+
+echo "MusicalInstruments"
+python createFileInformation.py MusicalInstruments $dataPath/MusicalInstruments
+
+echo "OfficeProducts"
+python createFileInformation.py OfficeProducts $dataPath/OfficeProducts
+
+echo "PatioLawnGarden"
+python createFileInformation.py PatioLawnGarden $dataPath/PatioLawnGarden
+
+echo "PetSupplies"
+python createFileInformation.py PetSupplies $dataPath/PetSupplies
+
+echo "SportsOutdoors"
+python createFileInformation.py SportsOutdoors $dataPath/SportsOutdoors
+
+echo "ToolsHomeImprovement"
+python createFileInformation.py ToolsHomeImprovement $dataPath/ToolsHomeImprovement
+
+echo "ToysAndGames"
+python createFileInformation.py ToysAndGames $dataPath/ToysAndGames
+
+# echo "VideoGames"
+# python createFileInformation.py VideoGames $dataPath/VideoGames
+
